3a76db2630855364307e4b247bff8a1fd41f6256
trewrad <trew.rad@gmail.com>
2022-02-14 16:30:03 +0530

    Added slow-mo workaround fix
    The workaround fix generates a clipped file, and THEN applies the slow motion filter
    This accomplishes rescaling-slowmotion combo and fixes the slow motion memory leak
    
    It was observed at this timescale x264 is much better at maintaining quality, and while much faster (0.09x vs 0.02x) nvenc can only produce 1MB/s streams (may require more investigation)

--- Trim.bat
+++ Trim.bat
@@ -5,6 +5,7 @@ set /p InTime=In Time:
 set /p OutTime=Out Time: 
 
 REM ffmpeg -ss %InTime% -i %1 %OutTtime% -c copy CLIP.mp4
+REM set retime=-vf "minterpolate='fps=120':'mi_mode=mci':'me_mode=bilat'",setpts=4*PTS setpts=8*PTS
 
 :start
 
@@ -62,21 +63,21 @@ goto next
 ECHO 1 - 2x
 ECHO 2 - 4x
 ECHO 3 - 8x
-set /p C=Time Factor? 
-IF %C%==1 GOTO 2x
-IF %C%==2 GOTO 4x
-IF %C%==3 GOTO 8x
+set /p Cslow=Time Factor? 
+IF %Cslow%==1 GOTO 2x
+IF %Cslow%==2 GOTO 4x
+IF %Cslow%==3 GOTO 8x
 
 :2x
-set retime=-vf minterpolate='fps=120':'mi_mode=blend',setpts=4*PTS
+set retime=-filter:v "minterpolate='fps=120':'mi_mode=mci',setpts=4*PTS"
 goto next
 
 :4x
-set retime=-vf minterpolate='fps=240':'mi_mode=blend',setpts=8*PTS
+set retime=-filter:v "minterpolate='fps=240',setpts=8*PTS"
 goto next
 
 :8x
-set retime=-vf minterpolate='fps=360':'mi_mode=blend',setpts=16*PTS
+set retime=-filter:v "minterpolate='fps=480',setpts=16*PTS"
 goto next
 
 :next
@@ -101,51 +102,70 @@ IF %M%==8 GOTO Vcustom
 
 :7M
 set /p fileName=Filename? 
-ffmpeg -hwaccel cuda -i %1 -ss !InTime! -to !OutTime!  -c:v h264_nvenc !rescale! !retime! -b:v 7M -preset slow -af aresample=async=1 "%fileName%.mp4"
+ffmpeg -hwaccel cuda -i %1 -ss !InTime! -to !OutTime!  -c:v h264_nvenc !rescale!  -b:v 7M -preset slow -af aresample=async=1 "%fileName%.mp4"
 pause
-exit
+
+goto next2
 
 :10M
 set /p fileName=Filename? 
-ffmpeg -hwaccel cuda -i %1  -ss !InTime! -to !OutTime! -af "aresample=async=1" -c:v h264_nvenc !rescale! !retime! -b:v  10M -preset slow "!fileName!.mp4"
+ffmpeg -hwaccel cuda -i %1  -ss !InTime! -to !OutTime! -af "aresample=async=1" -c:v h264_nvenc !rescale! -b:v  10M -preset slow "!fileName!.mp4"
 pause
-exit
+
+goto next2
 
 :30M
 set /p fileName=Filename? 
-ffmpeg -hwaccel cuda -i %1 -ss !InTime! -to !OutTime! -c:v h264_nvenc !rescale! !retime! -b:v 30M -preset slow -af aresample=async=1 "%fileName%.mp4"
-pause
-exit
+ffmpeg -hwaccel cuda -i %1 -ss !InTime! -to !OutTime! -c:v h264_nvenc !rescale! -b:v 30M -preset slow -af aresample=async=1 "%fileName%.mp4"
+
+
+
+goto next2
 
 :custom
 set /p fileName=Filename? 
 set /p customRate=Specify Custom Bitrate Value: 
-ffmpeg -hwaccel cuda -i %1 -ss !InTime! -to !OutTime! -c:v h264_nvenc !rescale! !retime! -b:v !customRate!M -preset slow -af aresample=async=1 "%fileName%.mp4"
+ffmpeg -hwaccel cuda -i %1 -ss !InTime! -to !OutTime! -c:v h264_nvenc !rescale! -b:v !customRate!M -preset slow -af aresample=async=1 "%fileName%.mp4"
 pause
-exit
+
+goto next2
 
 :V4M
 set /p fileName=Filename? 
-ffmpeg -hwaccel cuda -i %1 -ss !InTime! -to !OutTime! -c:v h264_nvenc !rescale! !retime! -rc:v vbr_hq -cq:v 26 -b:v 4M -maxrate:v 8M -profile:v high -af aresample=async=1 "%fileName%.mp4"
-exit
+ffmpeg -hwaccel cuda -i %1 -ss !InTime! -to !OutTime! -c:v h264_nvenc !rescale! -rc:v vbr_hq -cq:v 26 -b:v 4M -maxrate:v 8M -profile:v high -af aresample=async=1 "%fileName%.mp4"
+goto next2
 
 :V7M
 set /p fileName=Filename?
-ffmpeg -hwaccel cuda -i %1 -ss !InTime! -to !OutTime! -c:v h264_nvenc !rescale! !retime! -rc:v vbr_hq -cq:v 26 -b:v 7M -maxrate:v 14M -profile:v high -af aresample=async=1 "%fileName%.mp4"
-exit
+ffmpeg -hwaccel cuda -i %1 -ss !InTime! -to !OutTime! -c:v h264_nvenc !rescale! -rc:v vbr_hq -cq:v 26 -b:v 7M -maxrate:v 14M -profile:v high -af aresample=async=1 "%fileName%.mp4"
+goto next2
  
 :V10M
 set /p fileName=Filename? 
-ffmpeg -hwaccel cuda -i %1 -ss !InTime! -to !OutTime! -c:v h264_nvenc !rescale! !retime! -rc:v vbr_hq -cq:v 26 -b:v 10M -maxrate:v 20M -profile:v high -af aresample=async=1 "%fileName%.mp4"
-exit
+ffmpeg -hwaccel cuda -i %1 -ss !InTime! -to !OutTime! -c:v h264_nvenc !rescale! -rc:v vbr_hq -cq:v 26 -b:v 10M -maxrate:v 20M -profile:v high -af aresample=async=1 "%fileName%.mp4"
+goto next2
 
 :Vcustom
 set /p fileName=Filename? 
 set /p customRate=Specify Custom Bitrate Value:
-ffmpeg -hwaccel cuda -i %1 -ss !InTime! -to !OutTime! -c:v h264_nvenc !rescale! !retime! -rc:v vbr_hq -cq:v 26 -b:v 4M -maxrate:v 8M -profile:v high -af aresample=async=1 "%fileName%.mp4"
+ffmpeg -hwaccel cuda -i %1 -ss !InTime! -to !OutTime! -c:v h264_nvenc !rescale! -rc:v vbr_hq -cq:v 26 -b:v 4M -maxrate:v 8M -profile:v high -af aresample=async=1 "%fileName%.mp4"
 
+:next2
+IF '%choice%'=='Y' GOTO slowmoactual
+IF '%choice%'=='y' GOTO slowmoactual
+IF '%choice%'=='N' GOTO exit
+IF '%choice%'=='n' GOTO exit
+IF '%choice%'=='' GOTO exit
+
+:slowmoactual
+echo Slow Mo Processing will begin now
+echo ***Beware, this will use CPU and lots of RAM!***
+pause
+
+ffmpeg -i "%fileName%.mp4" !retime! "%fileName%-slowmo.mp4"
+del /f "%fileName%.mp4"
+pause
 exit
-
 
 REM ffmpeg -hwaccel cuda -i input.mp4 -c:v h264_nvenc -b:v 10M -preset slow -c:a copy output.mp4
 pause
